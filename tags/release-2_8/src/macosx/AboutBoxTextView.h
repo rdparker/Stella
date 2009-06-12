05f9fffd9f928dfc1a6f1613550ff05f815c5316
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2009-06-12 10:54:57 +0000

    Made tag for 2.8 release before I start on 2.8.1 stuff.

---
+++ tags/release-2_8/src/macosx/AboutBoxTextView.h
@@ -0,0 +1,13 @@
+/* AboutBoxTextView.h - Header for the
+   AboutBoxTextView view class for the
+   Macintosh OS X SDL port of Stella
+   Mark Grebe <atarimac@cox.net>
+*/
+/* $Id$ */
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutBoxTextView : NSTextView
+{
+}
+@end
