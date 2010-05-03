b3fa27a1f598162ea7453bd73a51915647a7f5ab
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2010-05-03 18:13:44 +0000

    Tagged 3.1.2 release.

---
+++ tags/release-3.1.2/src/gui/Launcher.hxx
@@ -0,0 +1,63 @@
+//============================================================================
+//
+//   SSSS    tt          lll  lll       
+//  SS  SS   tt           ll   ll        
+//  SS     tttttt  eeee   ll   ll   aaaa 
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2010 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//
+// $Id$
+//============================================================================
+
+#ifndef LAUNCHER_HXX
+#define LAUNCHER_HXX
+
+class Properties;
+class OSystem;
+
+#include "DialogContainer.hxx"
+
+/**
+  The base dialog for the ROM launcher in Stella.
+
+  @author  Stephen Anthony
+  @version $Id$
+*/
+class Launcher : public DialogContainer
+{
+  public:
+    /**
+      Create a new menu stack
+    */
+    Launcher(OSystem* osystem);
+
+    /**
+      Destructor
+    */
+    virtual ~Launcher();
+
+    /**
+      Initialize the video subsystem wrt this class.
+    */
+    bool initializeVideo();
+
+    /**
+      Wrapper for LauncherDialog::selectedRomMD5() method.
+    */
+    string selectedRomMD5();
+
+  private:
+    // The width and height of this dialog
+    uInt32 myWidth;
+    uInt32 myHeight;
+};
+
+#endif
