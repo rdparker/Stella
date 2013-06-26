5ae8a6f55d457cb37ec8ef045d16942d65de54a5
stephena <stephena@8b62c5a3-ac7e-4cc8-8f21-d9a121418aba>
2013-06-26 21:48:35 +0000

    Tagged 3.9 release.

---
+++ tags/release-3.9/src/debugger/gui/Cart3FWidget.cxx
@@ -0,0 +1,92 @@
+//============================================================================
+//
+//   SSSS    tt          lll  lll       
+//  SS  SS   tt           ll   ll        
+//  SS     tttttt  eeee   ll   ll   aaaa 
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2013 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//
+// $Id$
+//============================================================================
+
+#include "Cart3F.hxx"
+#include "PopUpWidget.hxx"
+#include "Cart3FWidget.hxx"
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Cartridge3FWidget::Cartridge3FWidget(
+      GuiObject* boss, const GUI::Font& font,
+      int x, int y, int w, int h, Cartridge3F& cart)
+  : CartDebugWidget(boss, font, x, y, w, h),
+    myCart(cart)
+{
+  uInt32 size = cart.mySize;
+
+  ostringstream info;
+  info << "Tigervision 3F cartridge, 2-256 2K banks\n"
+       << "Startup bank = " << cart.myStartBank << "\n"
+       << "First 2K bank selected by writing to $3F\n"
+       << "Last 2K always points to last 2K of ROM\n";
+
+  // Eventually, we should query this from the debugger/disassembler
+  uInt16 start = (cart.myImage[size-3] << 8) | cart.myImage[size-4];
+  start -= start % 0x1000;
+  info << "Bank RORG" << " = $" << HEX4 << start << "\n";
+
+  int xpos = 10,
+      ypos = addBaseInformation(size, "TigerVision", info.str()) + myLineHeight;
+
+  VariantList items;
+  for(uInt16 i = 0; i < cart.bankCount(); ++i)
+  {
+    const string& b = BSPF_toString(i);
+    items.push_back(b + " ($3F)");
+  }
+  ostringstream label;
+  label << "Set bank ($" << HEX4 << start << " - $" << (start+0x7FF) << "): ";
+  myBank =
+    new PopUpWidget(boss, font, xpos, ypos-2, font.getStringWidth("0 ($3F) "),
+                    myLineHeight, items, label.str(),
+                    font.getStringWidth(label.str()), kBankChanged);
+  myBank->setTarget(this);
+  addFocusWidget(myBank);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Cartridge3FWidget::loadConfig()
+{
+  myBank->setSelected(myCart.myCurrentBank);
+
+  CartDebugWidget::loadConfig();
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Cartridge3FWidget::handleCommand(CommandSender* sender,
+                                      int cmd, int data, int id)
+{
+  if(cmd == kBankChanged)
+  {
+    myCart.unlockBank();
+    myCart.bank(myBank->getSelected());
+    myCart.lockBank();
+    invalidate();
+  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+string Cartridge3FWidget::bankState()
+{
+  ostringstream& buf = buffer();
+
+  buf << "Bank = " << myCart.myCurrentBank << ", hotspot = $3F";
+
+  return buf.str();
+}
---
+++ tags/release-3.9/src/debugger/gui/DebuggerDialog.hxx
@@ -0,0 +1,106 @@
+//============================================================================
+//
+//   SSSS    tt          lll  lll       
+//  SS  SS   tt           ll   ll        
+//  SS     tttttt  eeee   ll   ll   aaaa 
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2013 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//
+// $Id$
+//
+//   Based on code from ScummVM - Scumm Interpreter
+//   Copyright (C) 2002-2004 The ScummVM project
+//============================================================================
+
+#ifndef DEBUGGER_DIALOG_HXX
+#define DEBUGGER_DIALOG_HXX
+
+class Debugger;
+class OSystem;
+class DialogContainer;
+class ButtonWidget;
+class CpuWidget;
+class PromptWidget;
+class RamWidget;
+class RomWidget;
+class TabWidget;
+class EditTextWidget;
+class TiaInfoWidget;
+class TiaOutputWidget;
+class TiaZoomWidget;
+class CartDebugWidget;
+
+#include "Dialog.hxx"
+#include "MessageBox.hxx"
+
+class DebuggerDialog : public Dialog
+{
+  public:
+    DebuggerDialog(OSystem* osystem, DialogContainer* parent,
+                   int x, int y, int w, int h);
+    ~DebuggerDialog();
+
+    PromptWidget& prompt()       { return *myPrompt;       }
+    TiaInfoWidget& tiaInfo()     { return *myTiaInfo;      }
+    TiaOutputWidget& tiaOutput() { return *myTiaOutput;    }
+    TiaZoomWidget& tiaZoom()     { return *myTiaZoom;      }
+    RomWidget& rom()             { return *myRom;          }
+    CartDebugWidget& cartDebug() { return *myCartDebug;    }
+    EditTextWidget& message()    { return *myMessageBox;   }
+    ButtonWidget& rewindButton() { return *myRewindButton; }
+
+    void loadConfig();
+    void handleKeyDown(StellaKey key, StellaMod mod, char ascii);
+    void handleCommand(CommandSender* sender, int cmd, int data, int id);
+
+    void showFatalMessage(const string& msg);
+
+  private:
+    enum {
+      kDDStepCmd      = 'DDst',
+      kDDTraceCmd     = 'DDtr',
+      kDDAdvCmd       = 'DDav',
+      kDDSAdvCmd      = 'DDsv',
+      kDDRewindCmd    = 'DDrw',
+      kDDExitCmd      = 'DDex',
+      kDDExitFatalCmd = 'DDer'
+    };
+
+    TabWidget *myTab, *myRomTab;
+
+    PromptWidget*    myPrompt;
+    TiaInfoWidget*   myTiaInfo;
+    TiaOutputWidget* myTiaOutput;
+    TiaZoomWidget*   myTiaZoom;
+    CpuWidget*       myCpu;
+    RamWidget*       myRam;
+    RomWidget*       myRom;
+    CartDebugWidget* myCartDebug;
+    EditTextWidget*  myMessageBox;
+    ButtonWidget*    myRewindButton;
+    GUI::MessageBox* myFatalError;
+
+  private:
+    void addTiaArea();
+    void addTabArea();
+    void addStatusArea();
+    void addRomArea();
+
+    void doStep();
+    void doTrace();
+    void doScanlineAdvance();
+    void doAdvance();
+    void doRewind();
+    void doExitDebugger();
+    void doExitRom();
+};
+
+#endif
